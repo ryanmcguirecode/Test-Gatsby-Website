996fe61bf2f243abb9cb7175984be1148e26d28d
Ryan McGuire <ryanmcguire@berkeley.edu>
2022-11-29 21:24:52 -0800

    about page text fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,12 @@ import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="About">
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>About</title>
 
 export default IndexPage
